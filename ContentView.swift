b32f37e965f468460e965db90bce9b1f8a31dce6
Nich <128910037+watermelonich@users.noreply.github.com>
2024-04-17 20:15:30 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  TaskManager
+//
+//  Created by Nich on 01/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var selection: TaskSection? = TaskSection.all
+
+    @State private var allTasks = Task.examples()
+    @State private var userCreatedGroups: [TaskGroup] = TaskGroup.examples()
+    @State private var searchTerm: String = ""
+    
+    var body: some View {
+        NavigationSplitView {
+            SidebarView(userCreatedGroups: $userCreatedGroups,
+                        selection: $selection)
+          
+        } detail: {
+            
+            if searchTerm.isEmpty {
+                switch selection {
+                    case .all:
+                        TaskListView(title: "All", tasks: $allTasks)
+                    case .done:
+                        StaticTaskListView(title: "All", tasks: allTasks.filter({ $0.isCompleted }))
+                    case .upcoming:
+                        StaticTaskListView(title: "All", tasks: allTasks.filter({ !$0.isCompleted }))
+                    case .list(let taskGroup):
+                        StaticTaskListView(title: taskGroup.title, tasks: taskGroup.tasks)
+                    case .none:
+                        Text("none")
+                }
+            } else {
+                StaticTaskListView(title: "All", tasks: allTasks.filter({ $0.title.contains(searchTerm) }))
+            }
+        }
+        .searchable(text: $searchTerm)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskManagerApp.swift
@@ -0,0 +1,49 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Nich on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .commands {
+            CommandMenu("Task") {
+                Button("Add new Task") {
+                    
+                }
+                .keyboardShortcut(KeyEquivalent("r"), modifiers: /*@START_MENU_TOKEN@*/.command/*@END_MENU_TOKEN@*/)
+            }
+            
+            CommandGroup(after: .newItem) {
+                Button("Add new Group") {
+                    
+                }
+            }
+        }
+        
+        WindowGroup("Special window") {
+            Text("special window")
+                .frame(minWidth: 200, idealWidth: 300, minHeight: 200)
+        }
+        .defaultPosition(.leading)
+        
+        Settings {
+            Text("Setting")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        
+        MenuBarExtra("Menu") {
+            Button("Do something amazing") {
+                
+            }
+        }
+    }
+    
+}
